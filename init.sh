a3ce525274f1c174bd14836b9dc58023cf98df18
vzaletn <vzaletny@gmail.com>
2018-04-21 17:14:14 +0000

    First changes

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-﻿sudo /etc/init.d/mysql start﻿
+sudo /etc/init.d/mysql start
+
