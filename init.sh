be7e4111e7edbba85b38cc0c3d4440358b677300
vzaletny <31381624+vzaletny@users.noreply.github.com>
2018-04-21 23:52:45 +0700

    Change

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
